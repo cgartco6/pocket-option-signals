c193165ed1613480b992f2f7931b0df0dc3133a9
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-07-09 20:24:53 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 TOKEN = "7928426674:AAElIFCM4hXJQNFvUKPcJFbmika2gohIAIc"  # Replace with your token
-CHAT_ID = "-1002672363651"  # Replace with your channel ID
+CHAT_ID = "-1002357030636"  # Replace with your channel ID
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
